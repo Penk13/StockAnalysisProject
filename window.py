90bedbe8d661d69a0736a41cb2368ec03421a09f
Penk13 <natanaelsteven109@gmail.com>
2021-06-03 15:24:50 +0700

    modify edit criteria display and delete criteria function

--- window.py
+++ window.py
@@ -8,8 +8,7 @@ import sqlite3
 # Create new window
 window = Tk()
 window.title('Stock Analysis')
-frame = LabelFrame(window, padx=20, pady=20)
-frame.pack(padx=10, pady=10)
+window.geometry('400x400')
 
 # Create a database or connect to one
 conn = sqlite3.connect('stock_database.db')
@@ -22,22 +21,31 @@ c.execute("""CREATE TABLE IF NOT EXISTS stock(
 			total int
 		)""")
 
+
+
+
 # --------------------------------- Main Function ---------------------------------
 # 1. Create show function
 def show():
 	# Create new window
 	window = Tk()
-	window.title('Edit')
+	window.title('Show')
 	frame = LabelFrame(window, padx=20, pady=20)
 	frame.pack(padx=10, pady=10)
-
-	# Show stock and criteria
-
 
 	# Create a database or connect to one
 	conn = sqlite3.connect('stock_database.db')
 	# Create a cursor
 	c = conn.cursor()
+
+
+	#TODO
+	# Show stock and criteria
+	# show table column
+	c.execute('SELECT * FROM stock')
+	column_list = [data[0] for data in c.description]
+	for i in range(2,len(column_list)):
+		Label(frame, text=column_list[i]).pack()
 
 	# Commit changes
 	conn.commit()
@@ -67,23 +75,35 @@ def edit():
 
 # 3. Create edit criteria function
 def edit_criteria():
+	# Create global variable for 3a and 3b function
+	global criteria_entry	# Get anything from criteria_entry for new column name in database in function 3a
+	global delete_num_entry	# Get id number to delete
+
 	# Create new window
 	window = Tk()
 	window.title('Edit Criteria')
-	frame = LabelFrame(window, padx=20, pady=20)
-	frame.pack(padx=10, pady=10)
+	window.geometry('750x400')
 
 	# ----- Display -----
-	criteria_label = Label(frame, text='Criteria').grid(row=0, column=0, pady=(0,5))
-	criteria_entry = Entry(frame, width=100)
-	criteria_entry.grid(row=0, column=1, columnspan=3)
+	# Frame 1 (Add Criteria)
+	frame = LabelFrame(window, text='Add Criteria', padx=20, pady=20)
+	frame.place(x=10, y=10)
 
-	add_criteria_btn = Button(frame, text='Add', command=add_criteria).grid(row=1, column=1, columnspan=3, ipadx=50)
+	criteria_label = Label(frame, text='Criteria').grid(row=0, column=0, padx=5, pady=10)
+	criteria_entry = Entry(frame, width=100)
+	criteria_entry.grid(row=0, column=1, columnspan=2, pady=10)
+	add_criteria_btn = Button(frame, text='Add', command=add_criteria).grid(row=1, column=1, columnspan=2, ipadx=50)
 	
-	delete_num_label = Label(frame, text='Delete Criteria Number').grid(row=2, column=1)
-	delete_num_entry = Entry(frame, width=3)
-	delete_num_entry.grid(row=2, column=2, sticky=W)
-	delete_criteria_btn = Button(frame, text='Delete', command=delete_criteria).grid(row=2, column=3, sticky=W)
+	# Frame 2 (Delete Criteria)
+	frame2 = LabelFrame(window, text='Delete Criteria', padx=20, pady=20)
+	frame2.place(x=10, y=150)
+
+	delete_num_label = Label(frame2, text='Delete Criteria Number').grid(row=0, column=0, padx=10, pady=10, sticky=E)
+	delete_num_entry = Entry(frame2, width=3)
+	delete_num_entry.grid(row=0, column=1, padx=10, pady=10, sticky=W)
+	crt_to_delete_btn = Button(frame2, text='Show', command=show_criteria).grid(row=0, column=2, sticky=W)
+	crt_to_delete_entry = Entry(frame2, width=100).grid(row=1, column=0, columnspan=3, pady=10) #TODO edit entry
+	delete_criteria_btn = Button(frame2, text='Delete', command=delete_criteria).grid(row=2, column=0, columnspan=3, padx=10, ipadx=50)
 
 	# Create a database or connect to one
 	conn = sqlite3.connect('stock_database.db')
@@ -96,21 +116,62 @@ def edit_criteria():
 	conn.close()
 
 # 3a. Create add criteria function
-# Add criteria to stock_database
 def add_criteria():
-	pass
+	# Create a database or connect to one
+	conn = sqlite3.connect('stock_database.db')
+	# Create a cursor
+	c = conn.cursor()
+
+	# Add criteria column to database
+	c.execute('ALTER TABLE stock ADD COLUMN {} text'.format(criteria_entry.get()))
+
+	# Delete criteria_entry box
+	criteria_entry.delete(0, END)
+
+	# Commit changes
+	conn.commit()
+	# Close connection
+	conn.close()
 
 # 3b. Create delete criteria function
-# Delete criteria from stock_database
 def delete_criteria():
-	pass
+	# Create a database or connect to one
+	conn = sqlite3.connect('stock_database.db')
+	# Create a cursor
+	c = conn.cursor()
 
+	#TODO
+	# Delete criteria column from database
+	c.execute('ALTER TABLE stock DROP COLUMN {}'.format('wow'))
 
+	# Commit changes
+	conn.commit()
+	# Close connection
+	conn.close()
+
+# 3c. Create show criteria function
+def show_criteria():
+	# Create a database or connect to one
+	conn = sqlite3.connect('stock_database.db')
+	# Create a cursor
+	c = conn.cursor()
+
+	#TODO
+	# Show criteria to entry
+	#c.execute('SELECT rowid FROM stock WHERE rowid ' + delete_num_entry.get())
+	#c.fetchone()
+
+	# Commit changes
+	conn.commit()
+	# Close connection
+	conn.close()
 
 
 
 # --------------------------------- Main Display ---------------------------------
 
+frame = LabelFrame(window, padx=20, pady=20)
+frame.place(x=10, y=10)
 
 # Create entry and label
 code_label = Label(frame, text='Code').grid(row=0, column=0, pady=(0,5))
